03f7323c79fa78a6acc7c15fa48ad06d4e9d6df4
YuriTu <tuqiang_ru@163.com>
2017-08-15 11:29:55 +0800

    😊

--- command/init.js
+++ command/init.js
@@ -1,35 +1,43 @@
-'ust strict'
-const { exec } = require("child_process")
-const co = require("co")
-const prompt = require("co-prompt")
-const config = require("../templates.json")
-const chalk = require("chalk")
+"ust strict"
+const { exec } = require("child_process");
+const co = require("co");
+const prompt = require("co-prompt");
+const chalk = require("chalk");
+
+const gitUrl = "https://github.com/MATRIX-RENN/templates-React-ES7.git";
 
 module.exports = () => {
     co(function* () {
-        let telName = yield prompt("templates name:")
-        let projectName = yield prompt("Project name:")
-        let gitUrl,branch;
+        let projectName = yield prompt("Project name:[my-app]");
+        let branch = yield prompt("Git branch:[master]");
+        projectName || (projectName = "my-app");
+        branch || (branch = "master");
 
-        if(!config.tpl[telName]){
-            console.log(chalk.red(" \n X Template not exit!"))
-        }
-        gitUrl = config.tpl[telName].url
-        branch = config.tpl[telName].branch
-
-        let cmdStr = `git clone ${gitUrl} && cd ${projectName} && git checkout ${branch}`;
+        const cmdStr = `git clone ${gitUrl} ${projectName}&& cd ${projectName} && git checkout ${branch}`;
 
         console.log(chalk.white(" \n Strart generating..."));
 
-        exec(cmdStr, (error,stdout,stderr) => {
-            if(error){
+        exec(cmdStr, (error, stdout,stderr) => {
+            if (error){
                 console.log("you has a error !!!")
                 console.log(error)
                 process.exit();
             }
-            console.log(chalk.green("\n conplete"))
-            console.log(` \n cd ${projectName} && npm install `)
-            process.exit()
-        })
-    })
-}
+            console.log(chalk.green(`
+                Success! Created ${projectName}.
+                Let's create react app in IE8.
+                
+                Inside that directory, you can run several commands:
+                  * npm run dev: Starts the development server.
+                  * npm run build: Bundles the app into dist for production.
+                
+                We suggest that you begin by typing:
+                  cd ${projectName}
+                  npm run build
+                
+                Enjoy yourself!`
+            ));
+            process.exit();
+        });
+    });
+};
